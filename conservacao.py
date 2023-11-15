5bef51420f6d86c0b351a34bb076f908f4f9fadf
bberaldo <biaberaldo@outlook.com>
2023-11-15 15:26:41 -0300

    B: interface gráfica

--- conservacao.py
+++ conservacao.py
@@ -1,23 +1,75 @@
 #Gestão de Dados de Conservação: Desenvolver um sistema que ordena e compara dados de conservação de espécies ameaçadas, como populações de animais ou vegetação nativa, para apoiar esforços de preservação.
 import openpyxl
+import tkinter as tk
+import time
 
-def selectionSort(array):
-  for i in range(len(array)):
-        
-    # setting min_indx equal to the first unsorted element
-    
-    min_indx = i
-    # Loop to iterate over un-sorted sub-array
-    for j in range(i+1, len(array)):
-    
-    #Finding the minimum element in the unsorted sub-array
-        if array[min_indx] > array[j]:
-            min_indx = j
-              
-    # swapping the minimum element with the element at min_index to place it at its correct position    
-    array[i], array[min_indx] = array[min_indx], array[i]
-    
-  print(array)
+def insertionSort(a):
+    start_time = time.time()
+
+    # traversing the array from 1 to length of the array(a)
+    for i in range(1, len(a)):
+  
+        temp = a[i]
+  
+        # Shift elements of array[0 to i-1], that are
+        # greater than temp, to one position ahead
+        # of their current position
+        j = i-1
+        while j >=0 and temp < a[j] :
+                a[j+1] = a[j]
+                j -= 1
+        a[j+1] = temp
+
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    return a, elapsed_time
+
+def particao(A, esquerda, direita):
+    # 1. Seleção do pivô. O pivô será o elemento A[esquerda].
+    pivo = A[esquerda]
+    # Particionamento do arranjo.
+    i = esquerda
+    j = direita
+    while i <= j:
+        # Encontra elemento maior que o pivo.
+        while A[i] <= pivo:
+            i += 1
+            if i == direita:
+                break
+
+        # Encontra elemento menor que o pivo.
+        while pivo <= A[j]:
+            j -= 1
+            if j == esquerda:
+                break
+
+        # Ponteiros i e j se cruzaram.
+        if i >= j:
+            break
+
+        # Troca elementos encontrados acima de lugar.
+        A[i], A[j] = A[j], A[i]
+
+    # Coloca o pivo no lugar certo.
+    pivo, A[j] = A[j], pivo
+
+    # j é o índice em que o pivo agora está.
+    return j
+
+def quicksort(A):
+  start_time = time.time()
+
+  if len(A) == 0:
+      result = A
+  else:
+      pivot = A[0]
+      frente = quicksort([menor for menor in A[1:] if menor <= pivot])
+      tras = quicksort([maior for maior in A[1:] if maior > pivot])
+      result = frente + [pivot] + tras
+
+  end_time = time.time()
+  elapsed_time = end_time - start_time
+  return result, elapsed_time
 
 def lerDados():
   try:
@@ -33,13 +85,9 @@ def lerDados():
     valores.append(cell_obj.value)
   
   print(valores)
-  selectionSort(valores)
-  print(valores)
-
-# mostrar valores
-  for i in range(2, valores + 1):
-    print(valores)
-     
+  return valores
+  # for i in range(len(valores)):
+  #    print(valores[i])     
 
 def menu():
   print("menu:")
@@ -49,4 +97,37 @@ def menu():
   except Exception as e:
     print("Valor inválido. Tente novamente:")
 
-lerDados()
+def actionBtnQuickSort():
+  array = lerDados()
+  dados_ordenados, tempo_quick = quicksort(array)
+  saidaQuickSort.insert(tk.END, f"Tempo do QuickSort: {tempo_quick:.6f} segundos\n")
+  saidaQuickSort.insert(tk.END, f"Dados Ordenados pelo QuickSort: {dados_ordenados}\n")
+
+def actionBtnInsertionSort():
+  array = lerDados()
+  dados_ordenados, tempo_insertion = insertionSort(array)
+  saidaInsertionSort.insert(tk.END, f"Tempo do InsertionSort: {tempo_insertion:.6f} segundos\n")
+  saidaInsertionSort.insert(tk.END, f"Dados Ordenados pelo InsertionSort: {dados_ordenados}\n")
+
+# variáveis globais de tempo de cada algoritmo
+# criando a janela
+janela = tk.Tk()
+janela.title("Algorítmos de Ordenação")
+janela.geometry("600x400")
+
+# Botão Quick Sort
+btnQuickSort = tk.Button(janela, text="QuickSort", command=actionBtnQuickSort)
+btnQuickSort.grid(row=1, column=0)
+
+# Botão Insertion Sort
+btnInsetionSort = tk.Button(janela, text="InsertionSort", command=actionBtnInsertionSort)
+btnInsetionSort.grid(row=1, column=1)
+
+saidaQuickSort = tk.Listbox(janela, height=300, width=40,)  # Ajuste a altura e largura conforme necessário
+saidaQuickSort.grid(row=2, column=0)
+
+saidaInsertionSort = tk.Listbox(janela, height=300, width=40)  # Ajuste a altura e largura conforme necessário
+saidaInsertionSort.grid(row=2, column=1)
+
+# Iniciar loop da janela
+janela.mainloop()
